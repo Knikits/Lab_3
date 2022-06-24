e3f28a46aacee8c935608bef12855002979b35aa
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-06-24 15:25:14 +0300

    add

--- main.cpp
+++ main.cpp
@@ -11,6 +11,16 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+/*
+    string type = "TypeGraph";
+    string To = "SomePath";
+    Printer printGraphA;
+    DataGraph dataGraph = printGraphA.getData();
+    GeneratorGraph generatorGraph;
+    Graph graph;
+    graph = generatorGraph.getGraph(dataGraph, "TypeGraph");
+    graph.PrintPdf(To);
+*/
     MainWindow w;
     w.show();
     return a.exec();
