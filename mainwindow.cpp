ebd0e6e64cd02f7a0de4d122dcf6d3752f7b3c92
Kostryukov Nikita <nikita.kostriukov@gmail.com>
2022-06-24 14:28:05 +0300

    OpenDirectory

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QFileDialog"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,5 +18,11 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString str = QFileDialog::getExistingDirectory();
+    qDebug() << str;
 }
 
